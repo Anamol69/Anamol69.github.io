eefd485fe66f087d89fbf5f444f9c1699503492b
Anamol69 <146878034+Anamol69@users.noreply.github.com>
2023-10-06 10:38:59 -0600

    randomized ball + made it phase

--- 02-ball-object/sketch.js
+++ 02-ball-object/sketch.js
@@ -4,36 +4,62 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-let theBall = {
-  x: 100,
-  y: 100,
-  radius: 25,
-  r: 255,
-  g: 0, 
-  b: 0,
-  dx: -4,
-  dy: -3,
-};
+
+let theBall;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  theBall = spawnBall();
 }
 
 function draw() {
   background(220);
   moveBall();
   displayBall();
+  keyTyped();
+}
+
+function keyTyped() {
+  if (key === " ") {
+    theBall = spawnBall();
+  }
+}
+
+function spawnBall() {
+  let theBall = {
+    x: random(width),
+    y: random(height),
+    radius: random(15, 20),
+    r: random(0, 255),
+    g: random(0, 255), 
+    b: random(0, 255),
+    dx: random(-5, 5),
+    dy: random(-5, 5),
+  };
+  return theBall;
 }
 
 function moveBall() {
   theBall.x += theBall.dx;
   theBall.y += theBall.dy;
-
-  if (theBall.x > width) {
+  
+  // off the right side
+  if (theBall.x - theBall.radius > width ) {
     theBall.x = 0 - theBall.radius;
   }
-  if (theBall.y > width) {
+  // off the left side
+  else if (theBall.x < 0 - theBall.radius) {
+    theBall.x = width + theBall.radius;
+  }
+  //off the bottom
+  if (theBall.y - theBall.radius > height) {
     theBall.y = 0 - theBall.radius;
   }
+  //off the top
+  else if (theBall.y < 0 - theBall.radius) {
+    theBall.y = height + theBall.radius;
+  }
+  console.log(theBall.x, theBall.y);
 
 
 }
