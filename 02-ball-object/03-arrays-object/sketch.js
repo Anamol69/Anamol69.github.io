2c7a7ae66085c5cf5c66b7eadebc6080b3e3e6f2
Anamol69 <146878034+Anamol69@users.noreply.github.com>
2023-10-11 11:38:21 -0600

    array project

---
+++ 02-ball-object/03-arrays-object/sketch.js
@@ -0,0 +1,21 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+let Jake = {
+  name: "Jake",
+  age: 16,
+  eyeColor: "blue"
+}
+let choice = [Jake, Jacob, Julian, Josh, James, Jeremy, Jarol, Jamal, Jace, Jordan, Jason, Jay, Jaclon, J];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
+
