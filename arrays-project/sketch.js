7d4e601ed78d2d0c3517991de9439885ad7bb58d
Anamol69 <146878034+Anamol69@users.noreply.github.com>
2023-10-18 12:13:35 -0600

    added transparent images + music and game start screen function

--- arrays-project/sketch.js
+++ arrays-project/sketch.js
@@ -8,20 +8,42 @@
 let questionMan;
 let answerMan;
 let court;
+let suspenseMusic;
+let gameStarted = false;
 
 function preload() {
   court = loadImage("court-room.jpg");
-  questionMan = loadImage("question-man.jpg");
+  questionMan = loadImage("question-man.png");
   answerMan = loadImage("answer-man.png");
+  suspenseMusic = loadSound("suspense-music.wav");
 
-
+  suspenseMusic.setVolume(0.5);
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  
 }
 
 function draw() {
   background(220);
+  if (gameStarted) {
+    // Start screen
+    fill(255);
+    textSize(32);
+    textAlign(CENTER, CENTER);
+    text("THE COURT", width / 2, height / 2 - 30);
+    textSize(18);
+    text("Press MOUSE to enter THE COURT!", width / 2, height / 2 + 30);
+  }
 }
+
+function screenPressed() {
+  if (!suspenseMusic.isPlaying()) {
+    suspenseMusic.loop();
+  }
+}
+
+
+
 
